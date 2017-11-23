00bb38152fcd5689135f6abf2f9fc6dc5850832f
donley828 <344919627@qq.com>
2017-11-23 19:48:22 +0800

    restruct index

---
+++ src/components/UIcomponents/sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+  <aside>
+    <ul class="nav-side">
+      <li><i class="fa fa-user-o" @click="toggleTab('imuser')"></i></li>
+      <li><i class="fa fa-comment-o" @click="toggleTab('imchat')"></i></li>
+      <li><i class="fa fa-users" @click="toggleTab('imnotice')"></i></li>
+      <li><i class="fa fa-align-justify"></i></li>
+    </ul>
+  </aside>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentTab: 'chat'
+    }
+  },
+  components: {
+    ImUser,
+    ImChat,
+    ImNotice
+  },
+  methods: {
+    toggleTab(tab) {
+      this.currentTab = tab
+    }
+  }
+}
+</script>
+<style scoped>
+aside {
+  height: 100%;
+  width: 90px;
+  background-color: #202225;
+}
+.nav-side li {
+  height: 90px;
+  line-height: 90px;
+  cursor: pointer;
+}
+.nav-side li:last-child {
+  width: 90px;
+  position: absolute;
+  bottom: 0;
+}
+.nav-side i {
+  color: #ffffff;
+  font-size: 30px;
+}
+.nav-side li:hover {
+  background-color: rgba(79, 84, 92, 0.6);
+}
+</style>
+
+
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,30 +1,10 @@
 <template>
-  <el-container>
-    <el-tabs :tab-position="tabPosition" v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="个人资料" name="first">
-        <span slot="label"><i class="fa fa-vcard"></i></span>
-        <my-profile></my-profile>
-      </el-tab-pane>
-      <el-tab-pane label="好友列表" name="second">
-        <span slot="label"><i class="fa fa-user"></i></span>
-        <my-chat></my-chat>
-      </el-tab-pane>
-      <el-tab-pane label="群组" name="third">
-        <span slot="label"><i class="fa fa-users"></i></span>
-        <my-group></my-group>
-      </el-tab-pane>
-      <el-tab-pane label="添加好友" name="fourth">
-        <span slot="label"><i class="fa fa-user-plus"></i></span>
-        <my-add></my-add>
-      </el-tab-pane>
-    </el-tabs>
-  </el-container>
+  <div class="container">
+    <side-bar></side-bar>
+  </div>
 </template>
 <script>
-import MyProfile from './page/profile'
-import MyAdd from './page/add'
-import MyChat from './page/chat'
-import MyGroup from './page/group'
+import SideBar from './UIcomponents/sidebar'
 export default {
   data() {
     return {
@@ -39,29 +19,16 @@ export default {
   },
   computed: {},
   components: {
-    MyProfile,
-    MyAdd,
-    MyChat,
-    MyGroup
+    SideBar
   }
 }
 </script>
 <style scoped>
-.el-container {
-  height: 500px;
-  border: 1px solid #eee;
-  flex-grow: 0.4;
-  flex-basis: 500px;
+.container {
+  width: 100%;
+  height: 100%;
   background-image: none;
   background-color: #ffffff;
-}
-.el-tabs {
-  height: 100%;
-  width: 100%;
-}
-.el-tabs i {
-  font-size: 20px;
-  padding: 20px 0;
 }
 </style>
 
